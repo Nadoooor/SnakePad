bb0c08898290ad88522348c7786c88382dd65ecd
Nader Sayed Ahmed Sayed Ahmed <nadersayed742@gmail.com>
2025-06-03 01:01:36 +0300

    Add The Project
    Forgot to work on the project while iam on the repo and worked on it on a local folder 👀

---
+++ Production/main.py
@@ -0,0 +1,35 @@
+
+import board
+
+
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner
+from kmk.keys import KC
+from kmk.modules.macros import Press, Release, Tap, Macros
+
+
+keyboard = KMKKeyboard()
+
+# Add the macro extension
+macros = Macros()
+keyboard.modules.append(macros)
+
+# Define your pins here!
+PINS = [board.GP3, board.GP4, board.GP29, board.GP1, board.GP2]
+
+# Tell kmk we are not using a key matrix
+keyboard.matrix = KeysScanner(
+    pins=PINS,
+    value_when_pressed=False,
+)
+
+# Here you define the buttons corresponding to the pins
+# Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
+# And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
+keyboard.keymap = [
+    [KC.A, KC.DELETE, KC.MACRO("Hello world!"), KC.Macro(Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD)),]
+]
+
+# Start kmk!
+if __name__ == '__main__':
+    keyboard.go()
